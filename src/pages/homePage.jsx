7be6f8c67262d8a96ca01db52967a835c0d8189a
anonymousd3vil-ops <anonymous.d3vil.07@gmail.com>
2026-07-28 00:52:21 +0530

    home page added

---
+++ src/pages/homePage.jsx
@@ -0,0 +1,12 @@
+import MainLayout from "../layout/mainLayout.jsx";
+
+function HomePage(){
+    return (
+        <MainLayout>
+            
+            
+        </MainLayout>
+    )
+}
+
+export default HomePage;
