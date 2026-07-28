2dd3a98fc41cc9d5410038fc915c786460a14267
anonymousd3vil-ops <anonymous.d3vil.07@gmail.com>
2026-07-28 14:14:28 +0530

    main layout updated

--- src/layout/mainLayout.jsx
+++ src/layout/mainLayout.jsx
@@ -1,35 +1,36 @@
+import Footer from "../components/footer";
+
 function MainLayout({children}) {
     return (
-        <div className="navbar bg-base-100 shadow-sm p-10">
-            <div className="flex-1">
-                <a className="btn btn-ghost text-xl">nitkechhore</a>
-            </div>
-            <div className="flex gap-2">
-                <input type="text" placeholder="Search" className="input w-24 md:w-auto" />
-                <div className="dropdown dropdown-end">
-                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                    <div className="w-10 rounded-full">
-                    <img
-                        alt="Tailwind CSS Navbar component"
-                        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-                    </div>
+        <div>
+            <div className="navbar bg-base-100 shadow-sm p-5 flex-col md:flex-row">
+                <div className="flex-1">
+                    <a className="btn btn-ghost text-xl rounded-lg">nitkechhore</a>
                 </div>
-                <ul
-                    tabIndex="-1"
-                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                    <li>
-                    <a className="justify-between">
-                        Profile
-                        <span className="badge">New</span>
-                    </a>
-                    </li>
-                    <li><a>Settings</a></li>
-                    <li><a>Logout</a></li>
-                </ul>
+                <div className="flex gap-2">
+                    <input type="text" placeholder="Search" className="input w-24 md:w-auto border rounded-lg" />
+                    <div className="dropdown dropdown-end">
+                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                        <div className="w-10 rounded-full">
+                        <img
+                            alt="Tailwind CSS Navbar component"
+                            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                        </div>
+                    </div>
+                    <ul
+                        tabIndex="-1"
+                        className="menu menu-sm dropdown-content bg-base-200 rounded-lg z-1 mt-3 w-60 p-2 shadow">
+                        <li><a>Profile</a></li>
+                        <li><a>Settings</a></li>
+                        <li><a>Logout</a></li>
+                    </ul>
+                    </div>
                 </div>
             </div>
             {children}
+            <Footer/>
         </div>
+            
     );
 }
 
