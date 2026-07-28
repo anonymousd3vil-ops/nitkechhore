085efcbd5bbef82d9e5da02c151eed2f2aeae899
anonymousd3vil-ops <anonymous.d3vil.07@gmail.com>
2026-07-28 14:14:05 +0530

    added diffrent constants for cards

---
+++ src/constants/CardArrays.js
@@ -0,0 +1,12 @@
+export const FeaturesCardArray = [
+  ["Notes", "Semester-wise study material", "📚"],
+  ["Coding", "DSA, Web Development, CP", "👨‍💻"],
+  ["Projects", "Student-made projects", "💡"],
+  ["Events", "Hackathons & College Events", "⌛"],
+  ["Opportunities", "Internships & Jobs", "💫"],
+  ["Community", "Connect with seniors & classmates", "💭"],
+];
+
+export const categories = [
+  ["Notes", "📚"], ["Coding", "👨‍💻"], ["UI/UX", "🌵"], ["AI", "🤖"], ["Placements", "🌟"], ["Projects", "💡"], ["Community", "💭"], ["Fun", "🎉"]
+]
